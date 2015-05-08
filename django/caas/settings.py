f15e3a7931a552f54c348ee80bfbd6c20016b8a9
Sean Collins <scollins36@gatech.edu>
2015-05-08 15:10:07 -0400

    Update INSTALLED_APPS to include djangotoolbox

--- django/caas/settings.py
+++ django/caas/settings.py
@@ -128,7 +128,8 @@ INSTALLED_APPS = (
     #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mongodb_engine' 
+    'django_mongodb_engine',
+    'djangotoolbox'
    # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:

fb89c5f9674e5f83d8dcdd916b975117d4144373
Sean Collins <scollins36@gatech.edu>
2015-05-08 15:38:40 -0400

    Update model with toString method

--- django/caas_app/models.py
+++ django/caas_app/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Catfact(models.Model):
-	_id = models.CharField(max_length=32)
-	text = models.CharField(max_length=4096)
+    _id = models.CharField(max_length=32)
+    text = models.CharField(max_length=4096)
+
+    def __unicode__(self):
+        return self.text
 
 class Meta(models.Model):
-	_id = models.CharField(max_length=32)
-	source = models.CharField(max_length=1024)
-	url = models.CharField(max_length=2000)
+    _id = models.CharField(max_length=32)
+    source = models.CharField(max_length=1024)
+    url = models.CharField(max_length=2000)
+
+    def __unicode__(self):
+        return "Source: " + self.source + ", URL: " + self.url
